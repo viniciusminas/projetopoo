1b286c11dda6e94a5f696783464ecf515c67f668
viniciusminas <viniciusminas_@hotmail.com>
2025-04-11 10:58:53 -0300

    Update PessoaController.java
    add entity pessoa

--- src/main/java/com/example/biblioteca/controller/PessoaController.java
+++ src/main/java/com/example/biblioteca/controller/PessoaController.java
@@ -3,18 +3,35 @@ package com.example.biblioteca.controller;
 import com.example.biblioteca.model.Pessoa;
 import com.example.biblioteca.repository.PessoaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/pessoas")
 public class PessoaController {
+
     @Autowired
     private PessoaRepository pessoaRepository;
 
     @GetMapping
-    public List<Pessoa> listarPessoas(){ return pessoaRepository.findAll(); }
+    public List<Pessoa> listarPessoas() {
+        return pessoaRepository.findAll();
+    }
+
+    @PostMapping
+    public Pessoa adicionarPessoa(@RequestBody Pessoa pessoa) {
+        return pessoaRepository.save(pessoa);
+    }
+
+    @GetMapping("/{id}")
+    public Pessoa buscarPessoa(@PathVariable Long id) {
+        return pessoaRepository.findById(id).orElse(null);
+    }
+
+    @DeleteMapping("/{id}")
+    public String removerPessoa(@PathVariable Long id) {
+        pessoaRepository.deleteById(id);
+        return "Pessoa removida com sucesso!";
+    }
 }
